adccad48868af158318a6f8005afba7f080ea7ae
Toor Shubhpreet <sstoor@toors-air.lan>
2024-07-10 17:10:22 -0500

    chore: convert project to TS from JS

--- src/index.tsx
+++ src/index.tsx
@@ -4,7 +4,11 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const rootElement = document.getElementById('root');
+
+// Type assertion to ensure non-null value
+const root = ReactDOM.createRoot(rootElement as HTMLElement);
+
 root.render(
   <React.StrictMode>
     <App />
